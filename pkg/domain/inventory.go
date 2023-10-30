09390c57aa755204c1c3616cca97a6c385a399ed
Arun K <arthurbishop120@gmail.com>
2023-10-30 23:10:37 +0530

    solved bug:checkout deleted payment method is also getting in response

--- pkg/domain/inventory.go
+++ pkg/domain/inventory.go
@@ -14,4 +14,5 @@ type Inventories struct {
 type Category struct {
 	ID       uint   `json:"id" gorm:"unique;not null"`
 	Category string `json:"category"`
+	Image    string `json:"category_image"`
 }
--- pkg/routes/userRoutes.go
+++ pkg/routes/userRoutes.go
@@ -63,7 +63,7 @@ func UserRoutes(engine *gin.RouterGroup,
 			profile.GET("/details", userHandler.GetUserDetails)
 			profile.GET("/address", userHandler.GetAddresses)
 			profile.POST("/address", userHandler.AddAddress)
-			profile.GET("/get-link", userHandler.GetMyReferenceLink)
+			profile.GET("/reference-link", userHandler.GetMyReferenceLink)
 
 			orders := profile.Group("/orders")
 			{
