2384b459752815754ccecefcaa4eb0c1cb44bd88
Arun K <arthurbishop120@gmail.com>
2023-11-08 22:48:52 +0530

    token fix

--- pkg/api/middleware/auth.go
+++ pkg/api/middleware/auth.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 )
@@ -9,11 +12,14 @@ func AdminAuthMiddleware(c *gin.Context) {
 
 	accessToken := c.Request.Header.Get("Authorization")
 
+	accessToken = strings.TrimPrefix(accessToken, "Bearer ")
+
 	_, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
 		return []byte("accesssecret"), nil
 	})
 	if err != nil {
 		// The access token is invalid.
+		fmt.Println("error catches here")
 		c.AbortWithStatus(401)
 		return
 	}
